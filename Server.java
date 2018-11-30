8e9c5c37ee8b739205c0e718de33d78917230a19
Jared Harmon <harmonjl@iu.edu>
2018-11-30 14:26:37 -0500

    just saving

--- Server.java
+++ Server.java
@@ -2,17 +2,16 @@
 
 //Author: Jared Harmon
 //File Created: 10/27/18
-//Last Editied: 10/29/18
+//Last Editied: 10/30/18
+
+//Version 1.0
 
 /* This class is the main class of the server
  *
- * It serves two main functions
- *
- * 	1: It listens for clients trying to connect, prompts them to enter a username
- * 	    then moves them to their requested room
- *
- * 	2: Has main control over the program allowing for creation/deletion of 
- * 	   rooms and deletion of users.
+ * The job of the server is to control and create all of the individual user threads
+ * The server keeps an array of size n of users
+ * When a user joins the server, it will notify all the previously created threads
+ * the server checks every loop for a user that has exited, will delete the thread, and notify the other threads
  */
 
 //Imported Classes
@@ -20,66 +19,109 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-class Server implements Runnable{
+class Server{
 
 	//inmutable class memebers
 	private final int PORT = 1234;
+	private final int MAX_USERS = 10;
 
-	//mutable class members
+	// class members
 	private ServerSocket server_socket;
 	private Socket new_connection;
-	//private Arraylist<User> users;	
-	//private Arraylist<Room> rooms;
-	private int num_of_users;
+	private User[] users;
+		
 
+	/*
+ 	* Server Constructor
+ 	*
+ 	* This constructor initalizes the server at given port and ip
+ 	* creats an array of users of size given 
+ 	* 
+ 	* it then calls the method listenForConnection
+ 	*/
+ 	
 	public Server(){
 
 		//init rooms, users and server_socket
 		try{
 
 			server_socket = new ServerSocket(PORT);
+			users = new User[MAX_USERS]; 
 
 		}catch(Exception e){
-		
+
 			System.out.println("Failed to create sever socket");		
 
 		}//end try
 
-		run();
+		listenForConnection();
+
 	}//end Server Constructor
 
-	public void run(){
+	/*
+ 	 * listen for connections
+ 	 *
+ 	 * this is the main while loop of the program
+ 	 *
+ 	 * This function listens for a connection
+ 	 * When a connection is established, it then adds a new user
+ 	 * loops back to listneing for a connection 
+ 	 *
+ 	 */ 
+	public void listenForConnection(){
 
 		while(true){
-			
-				listenForConnection();		
+
+			try{				
+
+				//listen for connection
+				System.out.println("Listening for connection...");
+				new_connection = server_socket.accept();
+				System.out.println("Connection established...");
+
+			        //create new user, notify all threads of creation
+			 	addUser();			
+
+			}catch(Exception e){
+				System.out.println("No conneciton...");
+			}//end try
 
 		}//end while
 
-	}//end run 
-
-	public int menu(){
-
-		return 0;
-
-	}//end menu
-
-	public void listenForConnection(){
-
-		try{
-			System.out.println("Listening for connection...");
-			new_connection = server_socket.accept();
-			System.out.println("Connection established...");
-		}catch(Exception e){
-	
-			System.out.println("No conneciton...");
-		}//end try
-
 	}//end listenForConnection
 
-	public static void main(String[] args){
+	/*
+ 	 * addUser method
+ 	 */ 
+	public void addUser(){
 
-		Server s = new Server();
-		
-	}//end main test	
+		for(int i = 0; i < MAX_USERS; i++){
+
+			if(users[i] == null){
+
+				users[i] = new User(new_connection, users);
+				notifyUsers(users[i]);					
+				users[i].start();
+				break;
+			
+			}//end if
+
+		}//end for	
+
+	}//end addUser
+
+	public void notifyUsers(User user){
+
+		for(int i = 0; i < MAX_USERS; i++){
+			
+			if(users[i] != null){
+
+				users[i].addUser(user);
+
+			}//end if 
+
+		}//end for
+	
+	}//end notify
+
 }//end Class Server
